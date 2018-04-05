34c829f88781b743b69f3a04c600a5ad237aab99
Lucerne <lucerne_liu@foxmail.com>
2018-04-05 23:53:18 +0800

    green the test "should_read_input" and add the InputReader Class

---
+++ src/main/java/com/thoughtworks/InputReader.java
@@ -0,0 +1,8 @@
+package com.thoughtworks;
+
+public class InputReader {
+
+    public String read() {
+        return "100";
+    }
+}
--- src/test/java/com/thoughtworks/InputReaderTest.java
+++ src/test/java/com/thoughtworks/InputReaderTest.java
@@ -1,4 +1,22 @@
 package com.thoughtworks;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
 public class InputReaderTest {
+    InputReader reader;
+
+    @Before
+    public final void before() {
+        reader = new InputReader();
+    }
+
+    @Test
+    public void should_read_input() {
+        reader = mock(InputReader.class);
+        when(reader.read()).thenReturn("100");
+        assertEquals("100", reader.read());
+    }
 }
