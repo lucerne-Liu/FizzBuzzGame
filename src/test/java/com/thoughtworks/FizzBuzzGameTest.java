be5ed671d6d34e3b1478cd8b29ab31b0261b6afa
Lucerne <lucerne_liu@foxmail.com>
2018-04-05 22:09:54 +0800

    red test1 without implement

--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,10 +1,22 @@
 package com.thoughtworks;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+
 public class FizzBuzzGameTest {
+    FizzBuzzGameTest game;
+
+    @Before
+    public void setup() {
+        game = new FizzBuzzGameTest();
+    }
+
     @Test
     public void should_return_Fizz_when_have_3() {
+        assertEquals("Fizz", game.replace(3));
 
     }
 }
